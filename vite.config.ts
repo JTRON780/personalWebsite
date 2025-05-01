47099982574538f55d7d43d9ed5c2c6fc74b1749
Johan L <61603601+JTRON780@users.noreply.github.com>
2025-05-01 02:04:31 -0400

    Update Website

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Copilot-Website/', // Set to your repo name
+  base: '/personalWebsite/', // Set to your repo name
   plugins: [react()],
 })
