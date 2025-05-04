e091016c72d5624c545bde851616611af8b9d918
Johan L <61603601+JTRON780@users.noreply.github.com>
2025-05-04 09:36:42 -0400

    Change base in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/personalWebsite/', // Set to your repo name
+  base: '/', // Changed from '/personalWebsite/' to '/' for custom domain
   plugins: [react()],
 })
